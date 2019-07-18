087390b7e6e3d52864b0a3a6fe849ed07be0c8ae
melybogarin <meliu91@gmail.com>
2019-07-19 00:36:06 +0200

    clase de diseno web

--- pendientes/views.py
+++ pendientes/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from pendientes.models import Tarea
+
+from random import randint
+
+
 # Create your views here.
 
 def index(request):
-    saludo = "Hola, Mundo! Esta es la raiz /"
-    return HttpResponse(saludo) #retornamos el saludo
+    listita = Tarea.objects.all()
+
+    persona = {
+    "nombre":"Melissa",
+    "edad":28,
+    "hobbies": ["caminar","salir a pasear con mi familia"],
+    "lista_tareas": listita,
+    }
+    return render(request, "inicio.html",persona)
+
+def tarea (request):
+    return HttpResponse("hola soy la vista de tarea")
+
+def music (request):
+    return HttpResponse(" hola estas buscando alguna cosa")
